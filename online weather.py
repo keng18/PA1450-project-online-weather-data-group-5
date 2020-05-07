67ca67f30bbe4b5071406cc85e1809563679c0c1
johan borg <56513637+johanborg1996@users.noreply.github.com>
2020-05-07 10:28:05 +0200

    first commit

---
+++ online weather.py
@@ -0,0 +1,63 @@
+import pandas as pd 
+import numpy as np 
+import csv
+
+
+def readfiles():
+
+    list_temperatur = []
+    with open('temp.csv', 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            list_temperatur.append(row)
+    print(list_temperatur)
+
+    list_sol = []
+    with open('solskenstid.csv', 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            list_sol.append(row)
+    print(list_sol)
+
+    list_neder = []
+    with open('nederbörd.csv', 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            list_neder.append(row)
+    print(list_neder)
+
+
+    return list_neder,list_sol,list_temperatur
+
+def temperature(list_temperatur):
+
+
+
+def sunshine(list_sol):
+
+
+def rain(list_neder):
+
+
+
+
+
+def main():
+    readfiles()=list_neder,list_sol,list_temperatur
+    answer = ""
+    while answer != "5":
+        str(input("what do you want to do?"))
+        print("1. Show temperature graph")
+        print("2. Show sunshine graph")
+        print("3. Show rain graph")
+
+        if answer == "1":
+            temperature(list_temperatur)
+        if answer == "2":
+            temperature(list_temperatur)
+        if answer == "3":
+            temperature(list_temperatur)
+
+main()
+
+
